18ccae3fab01144c74dac9ceedd9935cd0159d3f
lovill <lorenzo.villaggi@gmail.com>
2021-03-21 17:32:39 -0400

    Add FlowField class

---
+++ particles_noise_field/flowfield.py
@@ -0,0 +1,114 @@
+
+class FlowField(object):
+
+    def __init__(self, scl):
+
+        # size of grid cell
+        self._scl = scl
+
+        # get number of cols and rows
+        self._cols = floor(width / scl)
+        self._rows = floor(height / scl)
+        self._cells = self.init_2darray()
+        # self.init_noise()
+
+    def init_2darray(self):
+
+        arr = []
+
+        for x in range(self._cols):
+            arr.append([])
+
+            for y in range(self._rows): 
+                arr[x].append(None)
+
+        return arr
+
+    def init_noise(self, noise_seed=1, zoff=0):
+        """
+        Assign perlin noise angle values to cells
+        """
+        # noiseSeed(noise_seed)
+        inc = 0.01
+        yoff = 0
+
+        for y in range(self._rows):
+
+            xoff = 0
+
+            for x in range(self._cols):
+
+                noise_theta = noise(xoff, yoff, zoff) * TWO_PI * 4
+
+                self._cells[x][y] = noise_theta
+
+                xoff += inc
+
+            yoff += inc
+
+        # zoff += inc
+
+
+    def display_grid(self):
+
+        for y in range(self._rows):
+
+            for x in range(self._cols):
+
+                noFill()
+                stroke(0)
+                rect(
+                    x * self._scl, 
+                    y * self._scl, 
+                    self._scl, 
+                    self._scl
+                )
+
+    def display_field(self):
+
+        for y in range(self._rows):
+
+            for x in range(self._cols):
+
+                theta = self._cells[x][y]
+
+                push()
+                stroke(
+                    map(theta, 0, TWO_PI*4, 0, 255),
+                    0,
+                    map(theta, 0, TWO_PI*4, 255, 0)
+                )
+                noFill()
+                strokeWeight(1)
+                translate(x * self._scl, y * self._scl)
+                rotate(theta)
+                line(0, 0, self._scl, 0)
+                # circle(0, 0, self._scl)
+                pop()
+
+
+    def display_2d_perlinnoise(self):
+
+        inc = 0.05
+        yoff = 0
+
+        for y in range(self._rows):
+
+            xoff = 0
+            for x in range(self._cols):
+
+                noise_val = noise(xoff, yoff) * 255
+
+                fill(noise_val)
+                noStroke()
+                rect(
+                    x * self._scl, 
+                    y * self._scl, 
+                    self._scl, 
+                    self._scl
+                )
+
+                xoff += inc
+            yoff += inc
+
+
