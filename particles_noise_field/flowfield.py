4a1826657dc3dc613a890b6a025efc7e410d9f4c
lovill <lorenzo.villaggi@gmail.com>
2021-05-02 16:55:46 -0400

    Add curve display methods

--- particles_noise_field/flowfield.py
+++ particles_noise_field/flowfield.py
@@ -26,12 +26,19 @@ class FlowField(object):
 
         return arr
 
-    def init_noise(self, noise_seed=1, zoff=0):
+    def grid_size(self):
+
+        return (len(self._cells), len(self._cells[0]))
+
+    def init_noise(self, noise_seed=0, zoff=0):
         """
         Assign perlin noise angle values to cells
         """
-        # noiseSeed(noise_seed)
-        inc = 0.008
+        noiseSeed(noise_seed)
+        # inc = 0.0025
+        # inc = 0.005
+        # inc = 0.010
+        inc = 0.025
         # inc = 0.05
         # inc = map(mouseX, 0, width, 0.001, 0.5)
         yoff = 0
@@ -51,6 +58,81 @@ class FlowField(object):
             yoff += inc
 
         # zoff += inc
+
+    def display_curves(self, x, y, num_steps, max_steps=500):
+
+        stroke_weight = 0.5
+        hue = 240
+
+        push()
+        noFill()
+        colorMode(HSB, 360, 100, 100)
+        # stroke(
+        #     map(num_steps, 0, 1000, 220, 270),
+        #     map(num_steps, 0, 1000, 80, 100),
+        #     100
+        # )
+        
+        strokeWeight(stroke_weight)
+
+        # beginShape()
+        for n in range(num_steps):
+
+            # curveVertex(x, y)
+            stroke(
+                hue,
+                map(n, 0, max_steps, 0, 100),
+                map(n, 0, max_steps, 0, 100)
+            )
+            circle(x, y, stroke_weight)
+
+            x_offset = x 
+            y_offset = y 
+
+            column_index = int(x_offset / self._scl)
+            row_index = int(y_offset / self._scl)
+
+            #NOTE: normally you want to check the bounds here
+            if column_index >= self.grid_size()[0]:
+                column_index = self.grid_size()[0] - 1
+                continue
+            elif column_index <= - self.grid_size()[0]:
+                column_index = - (self.grid_size()[0] - 1)
+                continue
+
+            if row_index >= self.grid_size()[1]:
+                row_index = self.grid_size()[1] - 1
+                continue
+            elif row_index <= - self.grid_size()[1]:
+                row_index = - (self.grid_size()[1] - 1)
+                continue
+
+            # print("grid size:{}".format(self.grid_size()))
+            # print(column_index, row_index)
+
+            grid_angle = self._cells[column_index][row_index]
+
+            # step_length = width*0.0025
+            step_length = width*0.001
+            # step_length = width*0.00025
+
+            x_step = step_length * cos(grid_angle)
+            y_step = step_length * sin(grid_angle)
+            x = x + x_step
+            y = y + y_step
+        
+        # endShape()
+        pop()
+
+        colorMode(HSB, 360, 100, 100)
+        # stroke(hue, 100, 100)
+        stroke(
+            hue,
+            map(num_steps, 0, max_steps, 0, 100),
+            map(num_steps, 0, max_steps, 0, 100)
+        )   
+        strokeWeight(2)
+        circle(x, y, stroke_weight*1.25)
 
 
     def display_grid(self):
@@ -83,7 +165,7 @@ class FlowField(object):
                     map(theta, 0, NOISE_PI, 0, 10),
                     map(theta, 0, NOISE_PI, 0, 255),                    
                     map(theta, 0, NOISE_PI, 255, 0),
-                    255
+                    50
                 )
 
                 noFill()
--- particles_noise_field/particles_noise_field.pyde
+++ particles_noise_field/particles_noise_field.pyde
@@ -4,22 +4,55 @@ TOFF = 0.05
 
 def setup():
 
-    size(400, 400)
+    size(600, 600)
     # blendMode(BLEND)
     global flowfield
 
-    flowfield = FlowField(scl=5)
+    flowfield = FlowField(scl=10)
+
 
 def draw():
 
-    background(220)
+    colorMode(HSB, 360, 100, 100)
+    background(0)
     global TOFF
 
     # flowfield.display_grid()
     flowfield.init_noise(zoff=TOFF)
     
     # TOFF += map(mouseY, 0, height, 0.01, 0.5)
-    # TOFF += 0.001
-    flowfield.display_field()
+    TOFF += 0.001
+    # flowfield.display_field()
 
-    print(floor(frameRate))
+    y_steps = 70
+    x_steps = 70
+    max_steps = 500
+    for i in range(y_steps):
+        y_step = i / float(y_steps)
+
+        for j in range(x_steps):
+
+            x_step = j / float(x_steps)
+
+            # num_steps = int(map(y_step * x_step, 0, 1, 50, 800))
+            # num_steps = int(noise(x_step, y_step) * max_steps)
+            num_steps = int(random(0, 1) * max_steps)
+
+            flowfield.display_curves(width * x_step, height * y_step, num_steps, max_steps)
+
+    # n = 200
+    # for k in range(n):
+
+    #     x = cos((k/float(n)) * PI)*300 + width/2
+    #     y = sin((k/float(n)) * PI)*100 + 600
+
+    #     fill(0)
+    #     # circle(x, y, 2)
+
+    #     # num_steps = int(random(0, 1) * 1000)
+    #     num_steps = int(noise(x, y) * 6000)
+    #     flowfield.display_curves(x, y, num_steps)
+    
+    noLoop()
+
+    # print(floor(frameRate))
